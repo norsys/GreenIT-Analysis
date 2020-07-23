fde246ff79203051f9946014cda62f102f82dc29
Antoine Stalin <antoine.stalin@prestataires.boulanger.com>
2020-07-23 13:30:38 +0200

    test(httpError) : isolate test and fix to make it idempotent

--- script/rules/HttpError.js
+++ script/rules/HttpError.js
@@ -1,22 +1,26 @@
-rulesManager.registerRule({
-    complianceLevel: 'A',
-    id: "HttpError",
-    comment: "",
-    detailComment: "",
-    values : {
-      errorNumber : 0
-    },
-  
-    check: function (measures) {
-      if (measures.entries.length) measures.entries.forEach(entry => {
-        if (entry.response) {
-          if (entry.response.status >=400  ) {
-            this.detailComment += entry.response.status + " " + entry.request.url + "<br>";
-            this.values.errorNumber++;
-          }
+rulesManager.registerRule(createHttpErrorRule(), "harReceived");
+
+function createHttpErrorRule() {
+    return {
+        complianceLevel: 'A',
+        id: "HttpError",
+        comment: "",
+        detailComment: "",
+        values : {
+            errorNumber : 0
+        },
+
+        check: function (measures) {
+            if (measures.entries.length) measures.entries.forEach(entry => {
+                if (entry.response) {
+                    if (entry.response.status >=400  ) {
+                        this.detailComment += entry.response.status + " " + entry.request.url + "<br>";
+                        this.values.errorNumber++;
+                    }
+                }
+            });
+            if (this.values.errorNumber > 0) this.complianceLevel = 'C';
+            this.comment = chrome.i18n.getMessage("rule_HttpError_Comment", String(this.values.errorNumber));
         }
-      });
-      if (this.values.errorNumber > 0) this.complianceLevel = 'C';
-      this.comment = chrome.i18n.getMessage("rule_HttpError_Comment", String(this.values.errorNumber));
-    }
-  }, "harReceived");
+    };
+}
