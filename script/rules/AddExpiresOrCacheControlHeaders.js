8a00bdaa0a5d71bade09442e2a2197fe72d715c1
Antoine Stalin <antoine.stalin@prestataires.boulanger.com>
2020-07-23 13:44:41 +0200

    test(AddExpiresOrCacheControlHeaders) : isolate test and fix to make it idempotent

--- script/rules/AddExpiresOrCacheControlHeaders.js
+++ script/rules/AddExpiresOrCacheControlHeaders.js
@@ -1,34 +1,36 @@
-rulesManager.registerRule({
-    complianceLevel: 'A',
-    id: "AddExpiresOrCacheControlHeaders",
-    comment: "",
-    detailComment: "",
-    values : {
-        staticResourcesSize: 0,
-        staticResourcesWithCache: 0
-    },
+rulesManager.registerRule(createAddExpiresOrCacheControlHeadersRule(), "harReceived");
 
-    check: function (measures) {
+function createAddExpiresOrCacheControlHeadersRule() {
+    return {
+        complianceLevel: 'A',
+        id: "AddExpiresOrCacheControlHeaders",
+        comment: "",
+        detailComment: "",
+        values: {
+            staticResourcesSize: 0,
+            staticResourcesWithCache: 0
+        },
 
-        if (measures.entries.length) measures.entries.forEach(entry => {
-            if (isStaticRessource(entry)) {
-                this.values.staticResourcesSize += entry.response.content.size;
-                if (hasValidCacheHeaders(entry)) {
-                    this.values.staticResourcesWithCache += entry.response.content.size;
+        check: function (measures) {
+
+            if (measures.entries.length) measures.entries.forEach(entry => {
+                if (isStaticRessource(entry)) {
+                    this.values.staticResourcesSize += entry.response.content.size;
+                    if (hasValidCacheHeaders(entry)) {
+                        this.values.staticResourcesWithCache += entry.response.content.size;
+                    } else this.detailComment += chrome.i18n.getMessage("rule_AddExpiresOrCacheControlHeaders_DetailComment", `${entry.request.url} ${Math.round(entry.response.content.size / 100) / 10}`) + '<br>';
                 }
-                else this.detailComment += chrome.i18n.getMessage("rule_AddExpiresOrCacheControlHeaders_DetailComment",`${entry.request.url} ${Math.round(entry.response.content.size / 100) / 10}`) + '<br>';
-            }
-        });
+            });
 
-        if (this.values.staticResourcesSize > 0) {
-            const cacheHeaderRatio = this.values.staticResourcesWithCache / this.values.staticResourcesSize * 100;
-            if (cacheHeaderRatio < 95) {
-                if (cacheHeaderRatio < 90) this.complianceLevel = 'C'
-                else this.complianceLevel = 'B';
+            if (this.values.staticResourcesSize > 0) {
+                const cacheHeaderRatio = this.values.staticResourcesWithCache / this.values.staticResourcesSize * 100;
+                if (cacheHeaderRatio < 95) {
+                    if (cacheHeaderRatio < 90) this.complianceLevel = 'C'
+                    else this.complianceLevel = 'B';
+                } else this.complianceLevel = 'A';
+                this.comment = chrome.i18n.getMessage("rule_AddExpiresOrCacheControlHeaders_Comment",
+                    String(Math.round(cacheHeaderRatio * 10) / 10) + "%");
             }
-            else this.complianceLevel = 'A';
-            this.comment = chrome.i18n.getMessage("rule_AddExpiresOrCacheControlHeaders_Comment",
-                String(Math.round(cacheHeaderRatio * 10) / 10) + "%");
         }
     }
-}, "harReceived");  
+}
--- script/rules/CompressHttp.js
+++ script/rules/CompressHttp.js
@@ -1,32 +1,34 @@
-rulesManager.registerRule({
-    complianceLevel: 'A',
-    id: "CompressHttp",
-    comment: "",
-    detailComment: "",
-    values : {
-        compressibleResourcesSize : 0,
-        compressibleResourcesCompressedSize : 0
-    },
+rulesManager.registerRule(createCompressHttpRule(), "harReceived");
 
-    check: function (measures) {
-        if (measures.entries.length) measures.entries.forEach(entry => {
-            if (isCompressibleResource(entry)) {
-                this.values.compressibleResourcesSize += entry.response.content.size;
-                if (isResourceCompressed(entry)) {
-                    this.values.compressibleResourcesCompressedSize += entry.response.content.size;
+function createCompressHttpRule() {
+    return {
+        complianceLevel: 'A',
+        id: "CompressHttp",
+        comment: "",
+        detailComment: "",
+        values: {
+            compressibleResourcesSize: 0,
+            compressibleResourcesCompressedSize: 0
+        },
+
+        check: function (measures) {
+            if (measures.entries.length) measures.entries.forEach(entry => {
+                if (isCompressibleResource(entry)) {
+                    this.values.compressibleResourcesSize += entry.response.content.size;
+                    if (isResourceCompressed(entry)) {
+                        this.values.compressibleResourcesCompressedSize += entry.response.content.size;
+                    } else this.detailComment += chrome.i18n.getMessage("rule_CompressHttp_DetailComment", `${entry.request.url} ${Math.round(entry.response.content.size / 100) / 10}`) + '<br>';
                 }
-                else this.detailComment += chrome.i18n.getMessage("rule_CompressHttp_DetailComment",`${entry.request.url} ${Math.round(entry.response.content.size / 100) / 10}`) + '<br>';
+            });
+            if (this.values.compressibleResourcesSize > 0) {
+                const compressRatio = this.values.compressibleResourcesCompressedSize / this.values.compressibleResourcesSize * 100;
+                if (compressRatio < 95) {
+                    if (compressRatio < 90) this.complianceLevel = 'C'
+                    else this.complianceLevel = 'B';
+                } else this.complianceLevel = 'A';
+                this.comment = chrome.i18n.getMessage("rule_CompressHttp_Comment",
+                    String(Math.round(compressRatio * 10) / 10) + "%");
             }
-        });
-        if (this.values.compressibleResourcesSize > 0) {
-            const compressRatio = this.values.compressibleResourcesCompressedSize / this.values.compressibleResourcesSize * 100;
-            if (compressRatio < 95) {
-                if (compressRatio < 90) this.complianceLevel = 'C'
-                else this.complianceLevel = 'B';
-            }
-            else this.complianceLevel = 'A';
-            this.comment = chrome.i18n.getMessage("rule_CompressHttp_Comment",
-                String(Math.round(compressRatio * 10) / 10) + "%");
         }
     }
-}, "harReceived");
+}
