33dc08c4c8c7edc2b72c7d8ebe465b3bbacd697e
Antoine Stalin <antoine.stalin@prestataires.boulanger.com>
2020-07-27 09:40:32 +0200

    test(DomainsNumber) : isolate test and fix to make it idempotent

--- script/rules/DomainsNumber.js
+++ script/rules/DomainsNumber.js
@@ -1,27 +1,31 @@
-rulesManager.registerRule({
-    complianceLevel: 'A',
-    id: "DomainsNumber",
-    comment: "",
-    detailComment: "",
-    values : {
-        domains : []
-    },
+rulesManager.registerRule(createDomainsNumberRule(), "harReceived");
 
-    check: function (measures) {
-        if (measures.entries.length) measures.entries.forEach(entry => {
-            let domain = getDomainFromUrl(entry.request.url);
-            if (this.values.domains.indexOf(domain) === -1) {
-                this.values.domains.push(domain);
+function createDomainsNumberRule() {
+    return {
+        complianceLevel: 'A',
+        id: "DomainsNumber",
+        comment: "",
+        detailComment: "",
+        values: {
+            domains: []
+        },
+
+        check: function (measures) {
+            if (measures.entries.length) measures.entries.forEach(entry => {
+                let domain = getDomainFromUrl(entry.request.url);
+                if (this.values.domains.indexOf(domain) === -1) {
+                    this.values.domains.push(domain);
+                }
+            });
+            if (this.values.domains.length > 2) {
+                if (this.values.domains.length === 3) this.complianceLevel = 'B';
+                else this.complianceLevel = 'C';
             }
-        });
-        if (this.values.domains.length > 2) {
-            if (this.values.domains.length === 3) this.complianceLevel = 'B';
-            else this.complianceLevel = 'C';
-        }
-        this.values.domains.forEach(domain => {
-            this.detailComment += domain + "<br>";
-        });
+            this.values.domains.forEach(domain => {
+                this.detailComment += domain + "<br>";
+            });
 
-        this.comment = chrome.i18n.getMessage("rule_DomainsNumber_Comment", String(this.values.domains.length));
+            this.comment = chrome.i18n.getMessage("rule_DomainsNumber_Comment", String(this.values.domains.length));
+        }
     }
-}, "harReceived");
+}
