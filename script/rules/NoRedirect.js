848c566e0ff259a5ec85a345ff1a25ed8ae38b8f
Antoine Stalin <antoine.stalin@prestataires.boulanger.com>
2020-07-27 11:04:17 +0200

    test(NoRedirect & OptimizeSvg) : isolate test and fix to make it idempotent

--- script/rules/NoRedirect.js
+++ script/rules/NoRedirect.js
@@ -1,22 +1,26 @@
-rulesManager.registerRule({
-    complianceLevel: 'A',
-    id: "NoRedirect",
-    comment: "",
-    detailComment: "",
-    values :{
-      redirectNumber : 0
-    },
-  
-    check: function (measures) {
-      if (measures.entries.length) measures.entries.forEach(entry => {
-        if (entry.response) {
-          if (isHttpRedirectCode(entry.response.status)) {
-            this.detailComment += entry.response.status + " " + entry.request.url + "<br>";
-            this.values.redirectNumber++;
-          }
+rulesManager.registerRule(createNoRedirectRule(), "harReceived");
+
+function createNoRedirectRule() {
+    return {
+        complianceLevel: 'A',
+        id: "NoRedirect",
+        comment: "",
+        detailComment: "",
+        values: {
+            redirectNumber: 0
+        },
+
+        check: function (measures) {
+            if (measures.entries.length) measures.entries.forEach(entry => {
+                if (entry.response) {
+                    if (isHttpRedirectCode(entry.response.status)) {
+                        this.detailComment += entry.response.status + " " + entry.request.url + "<br>";
+                        this.values.redirectNumber++;
+                    }
+                }
+            });
+            if (this.values.redirectNumber > 0) this.complianceLevel = 'C';
+            this.comment = chrome.i18n.getMessage("rule_NoRedirect_Comment", String(this.values.redirectNumber));
         }
-      });
-      if (this.values.redirectNumber > 0) this.complianceLevel = 'C';
-      this.comment = chrome.i18n.getMessage("rule_NoRedirect_Comment", String(this.values.redirectNumber));
     }
-  }, "harReceived");
+}
--- script/rules/OptimizeSvg.js
+++ script/rules/OptimizeSvg.js
@@ -1,26 +1,30 @@
-rulesManager.registerRule({
-    complianceLevel: 'A',
-    id: "OptimizeSvg",
-    comment: chrome.i18n.getMessage("rule_OptimizeSvg_DefaultComment"),
-    detailComment: "",
-    values : {
-      totalSizeToOptimize: 0,
-      totalResourcesToOptimize: 0
-    },
-  
-    check: function (measures, resourceContent) {
-      if ((resourceContent.type === 'image') && isSvgUrl(resourceContent.url)) {
-        if (!isSvgOptimized(window.atob(resourceContent.content)))  // code is in base64 , decode base64 data with atob
-        {
-          this.detailComment += chrome.i18n.getMessage("rule_OptimizeSvg_detailComment",[resourceContent.url,String(Math.round(resourceContent.content.length / 100) / 10)]) + '<br>';
-          this.values.totalSizeToOptimize += resourceContent.content.length;
-          this.values.totalResourcesToOptimize++;
+rulesManager.registerRule(createOptimizeSvgRule(), "resourceContentReceived");
+
+function createOptimizeSvgRule() {
+    return {
+        complianceLevel: 'A',
+        id: "OptimizeSvg",
+        comment: chrome.i18n.getMessage("rule_OptimizeSvg_DefaultComment"),
+        detailComment: "",
+        values: {
+            totalSizeToOptimize: 0,
+            totalResourcesToOptimize: 0
+        },
+
+        check: function (measures, resourceContent) {
+            if ((resourceContent.type === 'image') && isSvgUrl(resourceContent.url)) {
+                if (!isSvgOptimized(window.atob(resourceContent.content)))  // code is in base64 , decode base64 data with atob
+                {
+                    this.detailComment += chrome.i18n.getMessage("rule_OptimizeSvg_detailComment", [resourceContent.url, String(Math.round(resourceContent.content.length / 100) / 10)]) + '<br>';
+                    this.values.totalSizeToOptimize += resourceContent.content.length;
+                    this.values.totalResourcesToOptimize++;
+                }
+                if (this.values.totalSizeToOptimize > 0) {
+                    if (this.values.totalSizeToOptimize < 20000) this.complianceLevel = 'B';
+                    else this.complianceLevel = 'C';
+                    this.comment = chrome.i18n.getMessage("rule_OptimizeSvg_Comment", String(this.values.totalResourcesToOptimize));
+                }
+            }
         }
-        if (this.values.totalSizeToOptimize > 0) {
-          if (this.values.totalSizeToOptimize < 20000) this.complianceLevel = 'B';
-          else this.complianceLevel = 'C';
-          this.comment = chrome.i18n.getMessage("rule_OptimizeSvg_Comment", String(this.values.totalResourcesToOptimize));
-        }
-      }
     }
-  }, "resourceContentReceived");
+}
