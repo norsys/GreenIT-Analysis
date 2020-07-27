359ba7c9b43116928b4f136b840ab1dbd79d4a7f
Antoine Stalin <antoine.stalin@prestataires.boulanger.com>
2020-07-27 11:18:21 +0200

    test(SocialNetworkButton) : isolate test and fix to make it idempotent

--- script/rules/SocialNetworkButton.js
+++ script/rules/SocialNetworkButton.js
@@ -1,27 +1,31 @@
-rulesManager.registerRule({
-    complianceLevel: 'A',
-    id: "SocialNetworkButton",
-    comment: chrome.i18n.getMessage("rule_SocialNetworkButton_DefaultComment"),
-    detailComment: "",
-    values : {
-        nbSocialNetworkButton : 0,
-        socialNetworks : []
-    },
+rulesManager.registerRule(createSocialNetworkButtonRule(), "harReceived");
 
-    check: function (measures) {
-        if (measures.entries.length) measures.entries.forEach(entry => {
-            const officalSocialButton = getOfficialSocialButtonFormUrl(entry.request.url);
-            if (officalSocialButton.length > 0) {
-                if (this.values.socialNetworks.indexOf(officalSocialButton) === -1) {
-                    this.values.socialNetworks.push(officalSocialButton);
-                    this.detailComment += chrome.i18n.getMessage("rule_SocialNetworkButton_detailComment", officalSocialButton) + "<br>";
-                    this.values.nbSocialNetworkButton++;
+function createSocialNetworkButtonRule() {
+    return {
+        complianceLevel: 'A',
+        id: "SocialNetworkButton",
+        comment: chrome.i18n.getMessage("rule_SocialNetworkButton_DefaultComment"),
+        detailComment: "",
+        values: {
+            nbSocialNetworkButton: 0,
+            socialNetworks: []
+        },
+
+        check: function (measures) {
+            if (measures.entries.length) measures.entries.forEach(entry => {
+                const officalSocialButton = getOfficialSocialButtonFormUrl(entry.request.url);
+                if (officalSocialButton.length > 0) {
+                    if (this.values.socialNetworks.indexOf(officalSocialButton) === -1) {
+                        this.values.socialNetworks.push(officalSocialButton);
+                        this.detailComment += chrome.i18n.getMessage("rule_SocialNetworkButton_detailComment", officalSocialButton) + "<br>";
+                        this.values.nbSocialNetworkButton++;
+                    }
                 }
+            });
+            if (this.values.nbSocialNetworkButton > 0) {
+                this.complianceLevel = 'C';
+                this.comment = chrome.i18n.getMessage("rule_SocialNetworkButton_Comment", String(this.values.nbSocialNetworkButton));
             }
-        });
-        if (this.values.nbSocialNetworkButton > 0) {
-            this.complianceLevel = 'C';
-            this.comment = chrome.i18n.getMessage("rule_SocialNetworkButton_Comment", String(this.values.nbSocialNetworkButton));
         }
     }
-}, "harReceived");
+}
